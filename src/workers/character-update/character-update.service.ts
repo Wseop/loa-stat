7e02768f3630ee6e6c68dc7f8335ceb44d97a690
Wseop <crazyz2000@naver.com>
2023-09-07 16:08:57 +0900

    feat:Worker-CharacterUpdateModule

---
+++ src/workers/character-update/character-update.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CharacterService } from 'src/character/character.service';
+import { ValidateCharacter } from 'src/character/functions/character.functions';
+import { LostarkService } from 'src/lostark/lostark.service';
+
+@Injectable()
+export class CharacterUpdateService {
+  private readonly logger: Logger = new Logger(CharacterUpdateService.name);
+
+  constructor(
+    private readonly lostarkService: LostarkService,
+    private readonly characterService: CharacterService,
+  ) {
+    setTimeout(() => {
+      this.run();
+    }, 1000 * 5);
+  }
+
+  private async getCharacterNames(): Promise<string[]> {
+    const result = await this.characterService.find(null, ['characterName']);
+
+    if (result)
+      return result.map((value) => {
+        return value.characterName;
+      });
+  }
+
+  private async updateCharacter(characterName: string) {
+    const result = await this.lostarkService.searchCharacter(characterName);
+
+    // 검증 후 upsert
+    if (result) {
+      if (ValidateCharacter(result)) {
+        await this.characterService.upsert(result);
+      }
+    } else {
+      // 더 이상 존재하지 않는 캐릭터면 db에서 삭제
+      await this.characterService.deleteOne(characterName);
+      this.logger.debug(`DELETE | ${characterName}`);
+    }
+  }
+
+  private async run() {
+    this.logger.log('START | CharacterUpdate');
+
+    while (true) {
+      const characterNames = await this.getCharacterNames();
+
+      while (characterNames.length > 0) {
+        await this.updateCharacter(characterNames.pop());
+        await new Promise((_) => setTimeout(_, 1000));
+      }
+    }
+  }
+}
--- src/workers/workers.module.ts
+++ src/workers/workers.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { CharacterCollectModule } from './character-collect/character-collect.module';
 import { ItemPriceModule } from './item-price/item-price.module';
 import { NoticeInformModule } from './notice-informer/notice-inform.module';
+import { CharacterUpdateModule } from './character-update/character-update.module';
 
 @Module({
-  imports: [CharacterCollectModule, ItemPriceModule, NoticeInformModule],
+  imports: [
+    CharacterCollectModule,
+    ItemPriceModule,
+    NoticeInformModule,
+    CharacterUpdateModule,
+  ],
 })
 export class WorkersModule {}
