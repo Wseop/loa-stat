f2907be83264de8ccdb346a6cb663543c5d023d7
Wseop <crazyz2000@naver.com>
2023-09-11 15:39:14 +0900

    feat:CacheWarmer

--- src/character/character.service.ts
+++ src/character/character.service.ts
@@ -56,15 +56,32 @@ export class CharacterService {
       let result: Character[] = await this.cacheManager.get(redisKey);
       if (!result) {
         // cache miss (db 접근, cache에 저장)
+        this.logger.debug(`Character cache miss - ${redisKey}`);
         if (classEngraving)
-          result = await this.find({ classEngraving }, [...fields]);
-        else result = await this.find(null, [...fields]);
+          result = await this.find({ classEngraving }, fields);
+        else result = await this.find(null, fields);
         this.cacheManager.set(redisKey, result, { ttl: 60 * 10 });
       }
       return result;
     } else {
       this.logger.error('RedisKey is null');
       return null;
+    }
+  }
+
+  async setCache(fields: string[], classEngraving: string, ttl: number) {
+    let redisKey = classEngraving;
+    fields.forEach((field) => (redisKey += field));
+
+    if (redisKey) {
+      let data;
+      if (classEngraving) data = await this.find({ classEngraving }, fields);
+      else data = await this.find(null, fields);
+
+      if (data) {
+        this.cacheManager.set(redisKey, data, { ttl });
+        this.logger.debug(`Set character cache - ${redisKey}`);
+      }
     }
   }
 
---
+++ src/workers/cache-warmer/cache-warmer.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CharacterService } from 'src/character/character.service';
+import { classEngravingMap } from 'src/lostark/consts/lostark.const';
+
+@Injectable()
+export class CacheWarmerService {
+  private readonly logger: Logger = new Logger(CacheWarmerService.name);
+
+  constructor(private readonly characterService: CharacterService) {
+    setTimeout(() => {
+      this.warmCharacterCache();
+    }, 1000 * 5);
+  }
+
+  private async warmCharacterCache() {
+    // 1시간마다 character 관련 cache warming
+    while (true) {
+      // server
+      await this.characterService.setCache(['serverName', 'itemLevel'], '', 0);
+      // classEngraving
+      await this.characterService.setCache(
+        ['classEngraving', 'itemLevel'],
+        '',
+        0,
+      );
+      // setting & skills
+      for (let classEngraving in classEngravingMap) {
+        await this.characterService.setCache(
+          ['setting', 'itemLevel'],
+          classEngraving,
+          0,
+        );
+        await this.characterService.setCache(
+          ['skills', 'itemLevel'],
+          classEngraving,
+          0,
+        );
+      }
+      this.logger.debug('Warming character cache end');
+      await new Promise((_) => setTimeout(_, 1000 * 60 * 60));
+    }
+  }
+}
