5e4ef76dc05d852b9a8cd80c9b3d1c9598d3897c
Wseop <crazyz2000@naver.com>
2023-09-13 09:30:19 +0900

    feat:disable CharacterCollect

--- src/workers/workers.module.ts
+++ src/workers/workers.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { CharacterCollectModule } from './character-collect/character-collect.module';
 import { ItemPriceModule } from './item-price/item-price.module';
 import { NoticeInformModule } from './notice-informer/notice-inform.module';
 import { CharacterUpdateModule } from './character-update/character-update.module';
@@ -7,7 +6,6 @@ import { CacheWarmerModule } from './cache-warmer/cache-warmer.module';
 
 @Module({
   imports: [
-    CharacterCollectModule,
     ItemPriceModule,
     NoticeInformModule,
     CharacterUpdateModule,
