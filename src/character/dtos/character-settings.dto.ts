de5d1466e21d31fdcb32ba974c72af6b5d9b386f
Wseop <crazyz2000@naver.com>
2023-09-25 18:57:55 +0900

    feat:캐릭터 세팅 swagger 수정

--- src/character/dtos/character-settings.dto.ts
+++ src/character/dtos/character-settings.dto.ts
@@ -7,35 +7,21 @@ export class CharacterSettingsDto extends CharacterSettings {
 
   @ApiProperty({
     type: Object,
-    properties: { statName: { type: 'number' } },
-    example: { 치신: 0 },
   })
   protected stat: { [key: string]: number };
 
   @ApiProperty({
     type: Object,
-    properties: {
-      setName: { type: 'number' },
-    },
-    example: { '6악몽': 0 },
   })
   protected set: { [key: string]: number };
 
   @ApiProperty({
     type: Object,
-    properties: {
-      elixirName: {
-        type: 'number',
-      },
-    },
-    example: {
-      회심: 0,
-    },
   })
   protected elixir: { [key: string]: number };
 
   @ApiProperty({
-    type: [Object],
+    type: Object,
   })
   protected engraving: { [key: string]: number };
 }
